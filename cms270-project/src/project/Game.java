0d68dbee0b66749fcdf69f55590f39196435b449
reneborr <reneborr@172.24.8.178>
2016-11-08 16:27:30 -0500

    Started playGame class

--- cms270-project/src/project/Game.java
+++ cms270-project/src/project/Game.java
@@ -1,9 +1,20 @@
 package project;
-
+import java.util.*;
 public class Game {
+	
+	public static void playGame(){
+		System.out.println("Please Enter your Name: ");
+		Scanner scan = new Scanner(System.in);
+		String name = scan.nextLine();
+		
+		Player player = new Player(name);
+		Player computer = new Player("Computer");
+		
+		
+		
+	}
 
 	public static void main(String[] args) {
-		Board board = new Board();
-		board.displayWithShips();
+		
 	}
 }
