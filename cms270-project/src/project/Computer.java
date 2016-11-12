f50394ad67344aa0353b0a74f8c976ca9655db7e
felixruiz <felixruiz@172.24.26.3>
2016-11-12 12:43:55 -0500

    Created random methods

--- cms270-project/src/project/Computer.java
+++ cms270-project/src/project/Computer.java
@@ -1,4 +1,6 @@
 package project;
+import java.util.*;
+
 
 public class Computer extends Player {
 
@@ -6,6 +8,26 @@ public class Computer extends Player {
 		super(); //call to superclass constructor 
 
 	}
+	
+	Random rand = new Random();
+	int start = 0;
+	int end = 10;
+	int randNum;
+	int randNum2;
+	char randColumn;
+	
+	public int returnRandomRow() { 
+		randNum = rand.nextInt(((end - start) +1)  + start);
+		return randNum;
+	}
+	
+	public char returnRandColumn() {
+		randNum2 = rand.nextInt(((end - start) +1)  + start);
+		char[] options = {'A','B','C','D','E','F','G','H','I','J'};
+		randColumn = options[randNum2];
+		return randColumn;
+	}
+	
 	
 	
 }
