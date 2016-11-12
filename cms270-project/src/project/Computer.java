3efae7b666492bc6b58850bb2a41eaacf9f7130e
felixruiz <felixruiz@172.24.26.3>
2016-11-12 14:41:19 -0500

    Overrided getRowAndColumn for Computer Class

--- cms270-project/src/project/Computer.java
+++ cms270-project/src/project/Computer.java
@@ -13,42 +13,19 @@ public class Computer extends Player {
 	int start = 0;
 	int end = 10;
 	int randNum;
-	int randNum2;
-	char randColumn;
+	char randRow;
 	boolean flag;
 	
-	public int getRow() { 
-		randNum = rand.nextInt(((end - start) +1)  + start);
-		return randNum;
-	}
-	
-	public char getColumn() {
-		randNum2 = rand.nextInt(((end - start) +1)  + start);
-		char[] options = {'A','B','C','D','E','F','G','H','I','J'};
-		randColumn = options[randNum2];
-		return randColumn;
-	}
-	
 	@Override
-	public boolean fire (Player player, char row, int column){
-		player.getMyBoard().getSquare(row, column).placeMissle();
-		if(player.getMyBoard().getSquare(row, column).hasShip()){
-			System.out.println("A ship has been struck!");
-			player.getShip(player.getMyBoard().getSquare(row, column).getShipNum()).hit();
-			return true;
-		}
-		if(!(player.getShip(player.getMyBoard().getSquare(row, column).getShipNum()).isAfloat())){
-			System.out.println("A ship has been sunk!");
-			player.sinkShip();
-			return false;
-		} 
-		else {
-			return false;
-		}
+	public void getRowAndColomn(char row, int column){
+		column = rand.nextInt(((end - start) +1)  + start);
+		randNum = rand.nextInt(((end - start) +1)  + start);
+		char[] options = {'A','B','C','D','E','F','G','H','I','J'};
+		randRow = options[randNum];
+		row = randRow;
 	}
 	
 	
-
 	
 	
 }
