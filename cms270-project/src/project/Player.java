0f58b8122f386a1e796a96a1d5e57028845cb55c
Rodman <Rodman@172.24.31.50>
2016-11-15 14:38:05 -0500

    Player class comments

--- cms270-project/src/project/Player.java
+++ cms270-project/src/project/Player.java
@@ -85,7 +85,8 @@ public class Player {
 		}
 		return true; 
 	}
-	//
+	//asks user for char input for row and checks to make sure 
+	//user enters a char
 	public char getRow(){
 		String line;
 		char row;
@@ -100,7 +101,8 @@ public class Player {
 		}
 		return row;
 	}
-	
+	//asks user for integer input and stores the value
+	//also does error checking for user input
 	public int getColumn(){
 		int column;
 		System.out.println("Please select a column (1-10)");
@@ -117,7 +119,8 @@ public class Player {
 
 	}
 
-
+	//takes a char and makes sure that it is one of
+	//the chars that corresponds to a real row
 	public static int charToNum(char c){
 		String alphabet = "ABCDEFGHIJ";
 		for(int i=0;i<alphabet.length();i++){
@@ -127,7 +130,7 @@ public class Player {
 		}
 		return -1;
 	} 
-
+	//places a ship for a player at a certain row and column
 	public boolean placeShip(Player player, Ship ship, char row, int column, Boolean vert){
 		char test = row;
 		if(vert==true && charToNum(row)+ship.getSize()>10){
