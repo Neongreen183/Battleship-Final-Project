98589618a7332cffdbea10224815a1ace05ddb7e
felixruiz <felixruiz@172.24.29.47>
2016-11-15 17:44:02 -0500

    update

--- cms270-project/src/project/Computer.java
+++ cms270-project/src/project/Computer.java
@@ -88,6 +88,7 @@ public class Computer extends Player {
 		}else if(vert==false && column+ship.getSize()>11){
 			//System.out.println("Sorry you cannot place this ship here");
 			return false;
+			
 		}
 		for(int i=0;i<ship.getSize();i++){
 			if(vert){
