cb99b37630f8c4d89480b120b721461edcc3f37d
reneborr <reneborr@172.24.9.152>
2016-11-15 21:08:08 -0500

    sortov fixed computer class

--- cms270-project/src/project/Computer.java
+++ cms270-project/src/project/Computer.java
@@ -7,17 +7,14 @@ import java.util.*;
  */
 public class Computer extends Player { 
 	Random rand = new Random();
-	int start = 0;
-	int end = 9;
 	int CColumn;
 	char CRow;
-	boolean flag;
+	
 	int turn;
 	List<Square> random = new ArrayList<Square>();
 
 	public Computer() {
 		super();//call to superclass constructor 
-		flag = false;
 		turn = 0;
 	}
 
@@ -46,38 +43,40 @@ public class Computer extends Player {
 	
 	@Override
 	public boolean fire (Player player, char row, int column){
+		if(turn > 0){
+			updateRowAndColumn();
+		}
 		if(player.getMyBoard().getSquare(row, column).hasMissle()){
-			//System.out.println("There is already a missle there!");
 			return false;
 		}
 		if(charToNum(row) == -1 || column<1 || column>10){
-			turn++;
+			
 			return false;
 		}
 		
 		player.getMyBoard().getSquare(row, column).placeMissle();
 
 		if(player.getMyBoard().getSquare(row, column).hasShip()){
-			if(turn>0){
-				updateRowAndColumn();
-			}
 			System.out.println("A ship has been struck!");
-			if(turn == 0)
-			{
+			if(turn == 0){
+				random = new ArrayList<Square>();
 				updateGuesses(row,column);
 				updateRowAndColumn();
 			}
 			
+			
+			
 			player.getShip(player.getMyBoard().getSquare(row, column).getShipNum()).hit();
-			flag = true;
+			
 			if(player.getShip(player.getMyBoard().getSquare(row, column).getShipNum()).isAfloat() == false){
 				System.out.println("A ship has been sunk!");
 				turn = 0;
 				player.sinkShip();
-				flag = false;
+				
 			} 
 		}else{
 			System.out.println("Miss! nothing was hit.");
+			
 		}
 		
 		return true; 
@@ -87,10 +86,10 @@ public class Computer extends Player {
 	public boolean placeShip(Player player, Ship ship, char row, int column, Boolean vert){
 		char test = row;
 		if(vert==true && charToNum(row)+ship.getSize()>10){
-			//System.out.println("Sorry you cannot place this ship there");
+			
 			return false;
 		}else if(vert==false && column+ship.getSize()>11){
-			//System.out.println("Sorry you cannot place this ship here");
+			
 			return false;
 			
 		}
@@ -98,13 +97,13 @@ public class Computer extends Player {
 			if(vert){
 
 				if(player.getMyBoard().getSquare(test, column).hasShip()==true){
-					//System.out.println("There is already a ship there");
+					
 					return false;
 				}
 				test++;
 			}else{
 				if(player.getMyBoard().getSquare(row, column+i).hasShip()==true){
-					//System.out.println("There is already a ship there");
+					
 					return false;
 				}
 			} 
@@ -123,8 +122,9 @@ public class Computer extends Player {
 	}
 	
 	public void updateGuesses( char row, int column) {
+		
 		if(column == 10){
-			random.add(getMyBoard().getSquare(row, column- 1));
+			random.add(getMyBoard().getSquare(row, column - 1));
 			random.add(getMyBoard().getSquare(row, column - 2));
 		}
 		else if(column == 1){
@@ -144,7 +144,7 @@ public class Computer extends Player {
 		else if(row == 74 || row == 106){
 			random.add(getMyBoard().getSquare((char)(row-1), column));
 			random.add(getMyBoard().getSquare((char)(row-2),column));
-			//row++;
+			
 		}
 		else{
 			random.add(getMyBoard().getSquare((char)(row+1), column));
@@ -155,15 +155,17 @@ public class Computer extends Player {
 		
 	}
 	public void updateRowAndColumn(){
-		if(turn == 4){
-			turn = 0;
-		}
-		turn++;
+		
 		CRow = random.get(turn).getRow();
 		CColumn = random.get(turn).getColumn();
+		turn++;
 		if(turn == 4){
 			turn = 0;
 		}
+		
+		
+		
+		
 	}
 	
 	public static int charToNum(char c){
