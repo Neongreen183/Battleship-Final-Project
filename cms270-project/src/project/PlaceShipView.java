ec5a3e8c4021c721be447516da14b195cb87aebd
LilJe <LilJe@172.24.3.17>
2016-12-02 18:55:40 -0500

    Corrected indentation and added Javadoc

--- cms270-project/src/project/PlaceShipView.java
+++ cms270-project/src/project/PlaceShipView.java
@@ -1,6 +1,6 @@
 package project;
 
-//import guis.SimpleMath;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Alert;
@@ -11,14 +11,17 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
+/**
+ * 
+ * @author Rene Borr, Felix Ruiz, Roderick Zak, Jerry Abril
+ * @version 1.0.0
+ */
 public class PlaceShipView extends VBox {
-	
+
 	private Label prompt;
 	private Label currentChoice;
 	private Label currentShip;
 	private Label prompt2;
-	//private TextField input;
 	private Button vertical;
 	private Button place;
 	private Button playGame;
@@ -31,11 +34,17 @@ public class PlaceShipView extends VBox {
 	private int turn = 0;
 	private Button continueOn;
 	Stage stage;
-	
+
+	/**
+	 * Opens up board for player to place ships
+	 * 
+	 * @param s The board that will be opened
+	 * @param game The game that will be played
+	 */
 	public PlaceShipView(Stage s, Game game) {
 		g = game;
 		stage = s;
-		
+
 		currentSize = 3;
 		vert = false;
 		BattleshipBoardView view = new BattleshipBoardView(g.getPlayerBoard());
@@ -43,12 +52,8 @@ public class PlaceShipView extends VBox {
 		column = view.getCurrentColumn();
 		prompt = new Label("Select a square.");
 		currentShip = new Label("Placing ship 1 (size 2)");
-		
-		
-
 		currentChoice = view.getLabel();
-		///input = new TextField();
-		
+
 		prompt2 = new Label ("Your ship will be placed the way the Button says. If you wish to change it\n"
 				+ "click on the button. ");
 		vertical = new Button("Horizonal");
@@ -61,10 +66,10 @@ public class PlaceShipView extends VBox {
 				{
 					vert = false;
 				}
-		        setVertical(e);
-		    }
+				setVertical(e);
+			}
 		});
-		
+
 		place = new Button("Place");
 		place.setOnAction(new EventHandler<ActionEvent> () {
 			@Override public void handle(ActionEvent e) {
@@ -72,40 +77,37 @@ public class PlaceShipView extends VBox {
 				column = view.getCurrentColumn();
 				placeShip(g);
 				view.updateBoard(g.getPlayerBoard());
-		    }
+			}
 		});
-		
-		
+
 		continueOn = new Button("Play game");
 		continueOn.setOnAction(new EventHandler<ActionEvent> () {
-		@Override public void handle(ActionEvent e) {
-			if(turn != 5){
-				launchErrorDialog("You must place all your ships before continueing");
+			@Override public void handle(ActionEvent e) {
+				if(turn != 5){
+					launchErrorDialog("You must place all your ships before continueing");
+				}
+				else{
+					return;
+				}
 			}
-			else{
-				return;
-			}
-	    }
-	});
-		
-		
+		});
 
+		//Adds the letters to the side of the board
 		VBox letters = new VBox();
 		Button label = new Button();
 		label.setText(" ");
-		//numbers.getChildren().addAll(label);
-			for (int i=0;i<10;i++){
-				label = new Button();
-				String alphabet = "ABCDEFGHIJ";
-				label.setText("" + alphabet.charAt(i));
-				label.setMaxWidth(view.getSize());
-				label.setMaxHeight(view.getSize());
-				label.setMinWidth(view.getSize());
-				label.setMinHeight(view.getSize());
-				letters.getChildren().addAll(label);
-			}
+		for (int i=0;i<10;i++){
+			label = new Button();
+			String alphabet = "ABCDEFGHIJ";
+			label.setText("" + alphabet.charAt(i));
+			label.setMaxWidth(view.getSize());
+			label.setMaxHeight(view.getSize());
+			label.setMinWidth(view.getSize());
+			label.setMinHeight(view.getSize());
+			letters.getChildren().addAll(label);
+		}
 
-		
+		//Adds the numbers to the top of the board
 		HBox numbers = new HBox();
 		label = new Button();
 		label.setText(" ");
@@ -123,81 +125,114 @@ public class PlaceShipView extends VBox {
 			label.setMinHeight(view.getSize());
 			numbers.getChildren().addAll(label);
 		}
-		
-		
 		HBox board = new HBox();
 		board.getChildren().addAll(letters,view);
-		
-		
 		getChildren().addAll(prompt,currentChoice,currentShip, numbers, board, prompt2, vertical, place);
 	}
-	
+
+	/**
+	 * Changes the text in the box depending on the action chose by player.
+	 * 
+	 * @param event Whether the player clicks on the button.
+	 */
 	protected void setVertical(ActionEvent event) {
 		if (vert == true){
 			vertical.setText("Vertical");
-		}
-		else{
+		}else{
 			vertical.setText("Horizonal");
 		}
 	}
-	
+
+	/**
+	 * Getter for whether ship is vertical or not.
+	 * 
+	 * @return Whether the ship is vertical or not.
+	 */
 	public Boolean getVert(){
 		return vert;
 	}
-	
+
+	/**
+	 * Getter for the ship's size.
+	 * 
+	 * @return What the size of the ship is.
+	 */
 	public int getCurrentSize(){
 		return currentSize;
 	}
-	
+
+	/**
+	 * Updates the display to show which ship is currently being placed.
+	 * 
+	 * @param num The number of the ship
+	 * @param size How big the ship is
+	 */
 	private void updateCurrentShip(int num, int size){
 		currentShip.setText("Placing ship " + num + " (size " + size + ")");
 	}
-	
+
+	/**
+	 * Launches an error box when one occurs
+	 * 
+	 * @param error Error display after one occurs.
+	 */
 	public void launchErrorDialog(String error) {
 		Alert alert = new Alert(AlertType.ERROR);
 		alert.setTitle("Error Dialog");
 		alert.setHeaderText(error);
 		alert.setContentText(error);
-
 		alert.showAndWait();
 	}
-	
-	
+
+	/**
+	 * Calls the player class to place the ship in the location they desire.
+	 * 
+	 * @param g The game being played
+	 */
 	public void placeShip(Game g ){
 		int [] shipSize = {2,3,3,4,5};
 		boolean success;
+		//Whether the placing of the ship in the location desired was valid. 
 		success = g.placeShip(g.getHumanPlayer(),shipSize[turn], row, column, vert);
 		if(success == true){
 			turn++;
-			//updateCurrentShip(turn+1,shipSize[turn]);
-		}
-		else{
+		}else{
 			launchErrorDialog("You Cannot place a ship there");
 		}
 		if(turn != 5){
 			updateCurrentShip(turn+1,shipSize[turn]);
-		}
-		else{
-			
+		}else{
 			stage.close();
 		}
 		
-		
+
 	} 
+	
+	/**
+	 * Returns the current turn
+	 * 
+	 * @return Who's turn it is.
+	 */
 	public int getTurn(){
 		return turn;
-		
+
 	}
 
+	/**
+	 * Returns the appropriate board view for the placing ships
+	 * 
+	 * @return The board that the player will place ships on.
+	 */
 	public BattleshipBoardView getView() {
 		return view;
 	}
 
+	/**
+	 * Sets the view to the appropriate size of the board.
+	 * 
+	 * @param view How big the view is for the board to fit.
+	 */
 	public void setView(BattleshipBoardView view) {
 		this.view = view;
 	}
-	
-	//Test
 }
-
-
