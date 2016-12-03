419cf6c4ee57bce69acf46b3c8315e57f3838b93
LilJe <LilJe@DESKTOP-VLA8NH5>
2016-12-02 22:56:19 -0500

    Corrected indentation and added Javadoc

--- cms270-project/src/project/Board.java
+++ cms270-project/src/project/Board.java
@@ -42,7 +42,14 @@ public class Board {
 	public Square getSquare(char row, int column){
 		return board[charToNum(row)][column-1];
 	}
-	
+
+	/**
+	 * Overloading previous method in case something is different.
+	 * 
+	 * @param row The row of a specific square.
+	 * @param column The column of a specific square.
+	 * @return Square value at a specific location.
+	 */
 	public Square getSquare(int row, int column){
 		return board[row][column];
 	}
@@ -58,7 +65,6 @@ public class Board {
 		for(int i = 0;i<10;i++){
 			System.out.print(alphabet.charAt(i));
 			for(int j = 0;j<10;j++){
-				
 				System.out.print(" " + board[i][j].getChar() + " ");
 			}
 			System.out.println();
@@ -78,11 +84,9 @@ public class Board {
 			for(int j = 0;j<10;j++){
 				if(board[i][j].getChar().equalsIgnoreCase("s")){
 					System.out.print(" - ");
-				}
-				else{
+				}else{
 					System.out.print(" " + board[i][j].getChar() + " ");
 				}
-
 			}
 			System.out.println("");
 		}
@@ -114,7 +118,16 @@ public class Board {
 		String alphabet = "ABCDEFGHIJ";
 		return alphabet.charAt(i);
 	}
-	
+
+	/**
+	 * Places the actual ships on the board by calling the ship class
+	 * 
+	 * @param row The row of where the ship goes
+	 * @param column The column where the ship goes
+	 * @param vert Whether the ship is vertical or not
+	 * @param size The size of the ship
+	 * @return Whether the placing was successful at doing so.
+	 */
 	public boolean placeShip(char row, int column, boolean vert, int size){
 		if(isValidShip(row,column,vert,size) == false){
 			return false;
@@ -130,35 +143,47 @@ public class Board {
 				column++ ;
 			}
 		}
-		
 		return true;
-		
 	}
-	
+
+	/**
+	 * Places a missile on a specific part of the board
+	 * 
+	 * @param row The row where the missile is going
+	 * @param column The column where the missile is going
+	 * @return Whether it was successful or not.
+	 */
 	public boolean placeMissle(char row, int column){
 		if(isValidMissle(row,column) == false){
 			return false;
 		}
-		
+
 		getSquare(row,column).placeMissle();
 		return true;
 	}
-	
+
+	/**
+	 * Checks to see whether the ship can be placed at the desired location.
+	 * 
+	 * @param row The row of the square
+	 * @param column The column of the square
+	 * @param vert Whether the ship on it is vertical
+	 * @param size The size of the ship
+	 * @return Whether it was possible or not.
+	 */
 	private boolean isValidShip(char row, int column, boolean vert, int size){
 		char test = row;
 		if(vert){
 			if(charToNum(row)+size>10){
 				return false;
 			}
-		}
-		else{
+		}else{
 			if(column+size>11){
 				return false;
 			}
 		}
 		for(int i=0;i<size;i++){
 			if(vert){
-
 				if(getSquare(test, column).getMyShip() != null){
 					//System.out.println("There is already a ship there");
 					return false;
@@ -173,19 +198,36 @@ public class Board {
 		}
 		return true;
 	}
-	
+
+	/**
+	 * Checks to see whether the missile on the square is valid
+	 * 
+	 * @param row The row of the square 
+	 * @param column The column of the square
+	 * @return Whether it's valid or not
+	 */
 	private boolean isValidMissle(char row, int column){
 		if (getSquare(row,column).hasMissle()){
 			return false;
 		}
 		return true;
-		
+
 	}
-	
+
+	/**
+	 * A method for holding all the ships and accessing them.
+	 * 
+	 * @return ArrayList of all the ships a player has.
+	 */
 	public ArrayList<Ship> getShips(){
 		return ships;
 	}
-	
+
+	/**
+	 * How many ships the player has left.
+	 * 
+	 * @return How many ships the player has left.
+	 */
 	public int getShipsLeft(){
 		int shipsLeft = 0;
 		for(int i=0;i<ships.size();i++){
@@ -195,10 +237,13 @@ public class Board {
 		}
 		return shipsLeft;
 	}
+	
+	/**
+	 * Generates a square on the board
+	 * 
+	 * @return The board made up of squares.
+	 */
 	public Square[][] getBoard(){
 		return board;
 	}
-	
-	//Test
-	
 }
